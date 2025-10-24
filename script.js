01a19ca070be463e6785e6890c6e8d708a49caa2
saulalexander2 <saulalexander257@gmail.com>
2025-10-23 22:08:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Particle {
     if(this.x<0||this.x>canvas.width)this.speedX*=-1;
     if(this.y<0||this.y>canvas.height)this.speedY*=-1;
   }
-  draw(){ ctx.fillStyle="rgba(0,255,0,0.5)"; ctx.beginPath(); ctx.arc(this.x,this.y,this.size,0,Math.PI*2); ctx.fill();}
+  draw(){ ctx.fillStyle="rgba(255,255,255,0.5)"; ctx.beginPath(); ctx.arc(this.x,this.y,this.size,0,Math.PI*2); ctx.fill();}
 }
 function init(){ particlesArray=[]; for(let i=0;i<70;i++){ particlesArray.push(new Particle()); } }
 function animate(){ ctx.clearRect(0,0,canvas.width,canvas.height); particlesArray.forEach(p=>{p.update();p.draw();}); requestAnimationFrame(animate);}
